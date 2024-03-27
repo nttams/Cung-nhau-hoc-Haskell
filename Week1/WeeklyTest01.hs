96230890bce3ea0d04d5b31e31d845c3c21415f6
tamhoang89 <tam.hvan@gmail.com>
2024-03-27 17:34:39 +0700

    Test question for Lesson 1-4

---
+++ Week1/WeeklyTest01.hs
@@ -0,0 +1,21 @@
+
+-- Question 1
+-- Write a multiline comment below.
+-- Viết một chú thích nhiều dòng.
+
+-- Question 2
+-- Define a function that takes a value and multiplies it by 3.
+-- Định nghĩa một hàm nhận một giá trị và nhân nó cho 3.
+
+-- Question 3
+-- Define a function that calculates the area of a circle.
+-- Định nghĩa một hàm tính diện tích của một hình tròn.
+
+-- Question 4
+-- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+-- Định nghĩa một hàm tính thể tích của một hình trụ bằng cách kết hợp hàm đã tạo ở bài trên với chiều cao của hình trụ.
+
+-- Question 5
+-- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
+-- Định nghĩa một hàm nhận chiều cao và bán kính của hình trụ và kiểm tra xem thể tích có lớn hơn hoặc bằng 42 không.
+
---
+++ Week4/WeeklyTest04.hs
@@ -0,0 +1,40 @@
+-- Question 1
+-- Lets say you have the nested values defined bellow. How would you get the value of
+-- 4 by using only pattern matching in a function?
+-- Giả sử bạn đã định nghĩa các giá trị lồng nhau như bên dưới. Bạn sẽ làm thế nào để lấy giá trị của
+-- 4 bằng cách chỉ sử dụng pattern matching trong một hàm?
+
+nested :: [([Int], [Int])]
+nested = [([1,2],[3,4]), ([5,6],[7,8])]
+
+-- Question 2
+-- Write a function that takes a list of elements of any type and, if the list has 3 or more elements, it
+-- removes them. Else, it does nothing. Do it two times, one with multiple function definitions and one with
+-- case expressions.
+{- Viết một hàm nhận một danh sách các phần tử thuộc bất kỳ kiểu dữ liệu nào, 
+     - Nếu danh sách có 3 phần tử trở lên, trả vể phần còn lại của danh sách với 3 phần tử đầu bị loại bỏ. 
+     - Nếu không, không làm gì cả, trả về danh sách nguyên vẹn.
+   Làm bằng hai cách, cách 1 với nhiều định nghĩa hàm và cách 2 với biểu thức case. -}
+
+-- Question 3
+-- Create a function that takes a 3-element tuple (all of type Integer) and adds them together
+-- Tạo một hàm nhận một bộ 3 phần tử (tất cả cùng kiểu Integer) và cộng chúng lại.
+
+
+-- Question 4
+-- Implement a function that returns True if a list is empty and False otherwise.
+-- Viết một hàm trả về True nếu nhận vào một danh sách rỗng và False nếu ngược lại.
+
+
+-- Question 5
+-- Write the implementation of the tail function using pattern matching. But, instead of failing if
+-- the list is empty, return an empty list.
+-- Viết lại hàm tail bằng cách sử dụng pattern matching. Nhưng, thay vì gặp lỗi nếu
+-- danh sách là rỗng, thì trả về một danh sách rỗng cho trường hợp đó.
+
+
+-- Question 6
+-- write a case expression wrapped in a function that takes an Int and adds one if it's even. Otherwise does nothing. 
+-- (Use the `even` function to check if the number is even.)
+-- Viết hàm dùng biểu thức case: Nhận một số Int, và thêm 1 vào nó nếu nó là số chẵn. Ngược lại, không làm gì cả.
+-- (Sử dụng hàm even để kiểm tra xem số đó có là số chẵn không.)
